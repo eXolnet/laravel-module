36ca538809ba02728f8a36cb53b0ac82e4e6f145
Tom Rochette <trochette@exolnet.com>
2014-06-18 12:39:23 -0400

    Setup our default local/staging/testing environment detection.

--- Core/EnvironmentDetector.php
+++ Core/EnvironmentDetector.php
@@ -2,12 +2,27 @@
 
 class EnvironmentDetector
 {
+	private static $environments = [
+		'development.exolnet.com' => 'local',
+		'staging.exolnet.com'     => 'staging',
+		'testing.exolnet.com'     => 'testing',
+	];
+
 	public static function detect($environments)
 	{
 		// TODO: Do not allow $is_behat = true in production environment
 		$is_behat = self::is_remote_test();
 		if ($is_behat) {
 			return function() { return 'test'; };
+		}
+
+		if (isset($_SERVER['HTTP_HOST']))
+		{
+			$host = $_SERVER['HTTP_HOST'];
+
+			if (array_key_exists($host, self::$environments)) {
+				return function() use ($host) { return self::$environments[$host]; };
+			}
 		}
 
 		return $environments;
