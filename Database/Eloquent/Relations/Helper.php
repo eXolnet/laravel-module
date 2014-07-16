980346836b09142eabfb1b0ed9d32624362ca034
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2014-07-16 15:28:16 -0400

    Add a Helper class for model relations

---
+++ Database/Eloquent/Relations/Helper.php
@@ -0,0 +1,78 @@
+<?php namespace Exolnet\Database\Eloquent\Relations;
+/**
+ * @copyright  Copyright (c) 2014 eXolnet
+ * @author     eXolnet Inc. <info@exolnet.com>
+ * @package    Exolnet
+ * @subpackage Database
+ */
+
+use Closure;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Helper
+{
+	public static function syncHasMany(HasMany $relation, array $items, Closure $onSave = null)
+	{
+		$updated_keys = [];
+		$new_items    = [];
+
+		// 1. Update existing relations and prepare the delete and create steps
+		foreach ($items as $item) {
+			$model = static::getRelatedModel($relation, $item);
+
+			if ($model === null) {
+				$new_items[] = $item;
+				continue;
+			}
+
+			// On met à jour la relation
+			$model->fill($item)->save();
+
+			if ($onSave) {
+				$onSave($model, $item);
+			}
+
+			$updated_keys[] = $model->getKey();
+		}
+
+		// 2. Delete old relations
+		static::deleteAllRelatedExcept($relation, $updated_keys);
+
+		// 3. Create new relations
+		foreach ($new_items as $item) {
+			$model = $relation->create($item);
+
+			if ($onSave) {
+				$onSave($model, $item);
+			}
+		}
+	}
+
+	protected static function getRelatedModel(Relation $relation, array $item)
+	{
+		$related  = $relation->getRelated();
+		$key_name = $related->getKeyName();
+		$key      = array_get($item, $key_name, null);
+
+		if ($key === null) {
+			return null;
+		}
+
+		return $related->find($key);
+	}
+
+	protected static function deleteAllRelatedExcept(Relation $relation, $excluded_ids = [])
+	{
+		$related  = $relation->getRelated();
+		$key_name = $related->getKeyName();
+		$query    = $relation->getQuery();
+
+		if (count($excluded_ids) > 0) {
+			$query->whereNotIn($key_name, $excluded_ids);
+		}
+
+		$query->delete();
+	}
+}
