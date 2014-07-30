8fbf82cd92e98cca87a35cc00c11fa7fcdb5dd45
Martin Blanchette <mblanchette@exolnet.com>
2014-07-30 16:49:09 -0400

    Merge branch

---
+++ Core/Arr.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martinblanchette
+ * Date: 2014-07-30
+ * Time: 11:38
+ */
+
+namespace Exolnet\Core;
+
+
+class Arr {
+	public static function pluck($array, $value, $key = null)
+	{
+		$results = array();
+
+		foreach ($array as $item)
+		{
+			$itemValue = is_object($item) ? object_get($item, $value) : array_get($item, $value);
+
+			// If the key is "null", we will just append the value to the array and keep
+			// looping. Otherwise we will key the array using the value of the key we
+			// received from the developer. Then we'll return the final array form.
+			if (is_null($key))
+			{
+				$results[] = $itemValue;
+			}
+			else
+			{
+				$itemKey = is_object($item) ? object_get($item, $key) : array_get($item, $key);
+
+				$results[$itemKey] = $itemValue;
+			}
+		}
+
+		return $results;
+	}
+}
