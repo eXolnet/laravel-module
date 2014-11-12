fd80b1f54e3527f7775a8b645d03d704fa7f1dc3
Tom Rochette <trochette@exolnet.com>
2014-11-12 15:09:48 -0500

    Added buildUniqueRules (from Ardent) in the ValidationTrait.

--- Database/Eloquent/ValidationTrait.php
+++ Database/Eloquent/ValidationTrait.php
@@ -33,6 +33,7 @@ trait ValidationTrait {
 	public function getRules($attributes = null)
 	{
 		$rules = isset($this->rules) ? $this->rules : [];
+		$rules = $this->buildUniqueRules($rules);
 
 		if ($attributes !== null) {
 			if ( ! is_array($attributes)) {
@@ -132,5 +133,56 @@ trait ValidationTrait {
 		$this->shouldBeValid();
 
 		return parent::performInsert($query);
+	}
+
+	/**
+	 * Note: Code taken from Ardent::buildUniqueExclusionRules.
+	 * @param array $rules
+	 */
+	protected function buildUniqueRules(array $rules = [])
+	{
+		if ( ! $this->exists) {
+			return $rules;
+		}
+
+		foreach ($rules as $field => &$ruleset) {
+			// If $ruleset is a pipe-separated string, switch it to array
+			$ruleset = (is_string($ruleset))? explode('|', $ruleset) : $ruleset;
+
+			foreach ($ruleset as &$rule) {
+			  if (strpos($rule, 'unique') === 0) {
+				$params = explode(',', $rule);
+
+				$uniqueRules = array();
+
+				// Append table name if needed
+				$table = explode(':', $params[0]);
+				if (count($table) === 1) {
+					$uniqueRules[1] = $this->table;
+				} else {
+					$uniqueRules[1] = $table[1];
+				}
+
+				// Append field name if needed
+				if (count($params) === 1) {
+					$uniqueRules[2] = $field;
+				} else {
+					$uniqueRules[2] = $params[1];
+				}
+
+				if (isset($this->primaryKey)) {
+					$uniqueRules[3] = $this->{$this->primaryKey};
+					$uniqueRules[4] = $this->primaryKey;
+				} else {
+					$uniqueRules[3] = $this->id;
+				}
+
+				$rule = 'unique:' . implode(',', $uniqueRules);
+			  } // end if strpos unique
+
+			} // end foreach ruleset
+		}
+
+		return $rules;
 	}
 }
