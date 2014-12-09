3c225d963205130e5e882286de5ea25d17e9ce22
Tom Rochette <trochette@exolnet.com>
2014-12-09 15:49:11 -0500

    Call PHP setlocale() when App::setLocale is called with a supported locale (configured in app/config/locale-mapping.php).

--- Foundation/Application.php
+++ Foundation/Application.php
@@ -42,4 +42,29 @@ class Application extends LaravelApplication {
 		$this->loadedProviders = [];
 		$this->deferredServices = [];
 	}
+
+	/**
+	 * Set the current application locale.
+	 *
+	 * @param  string  $locale
+	 * @return void
+	 */
+	public function setLocale($locale)
+	{
+		parent::setLocale($locale);
+
+		$localeMapping = $this['config']->get('locale-mapping');
+
+		$mappedValue = array_get($localeMapping, 'locales.'.$locale);
+		if ($mappedValue) {
+			$mappedCategories = array_get($localeMapping, 'categories');
+			if (in_array(LC_ALL, $mappedCategories)) {
+				setlocale(LC_ALL, $mappedValue);
+			} else {
+				foreach ($mappedCategories as $mappedCategory) {
+					setlocale($mappedCategory, $mappedValue);
+				}
+			}
+		}
+	}
 }
