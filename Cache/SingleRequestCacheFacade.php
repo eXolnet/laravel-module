ac2925e1241734ed5daaff5c07186adb212d1a40
Tom Rochette <roctom@gmail.com>
2015-03-02 21:54:53 -0500

    Strip all copyright headers in preparation for MIT license release.

--- Cache/SingleRequestCacheFacade.php
+++ Cache/SingleRequestCacheFacade.php
@@ -1,19 +1,4 @@
 <?php namespace Exolnet\Cache;
-/**
- * Copyright © 2014 eXolnet Inc. All rights reserved. (http://www.exolnet.com)
- *
- * This file contains copyrighted code that is the sole property of eXolnet Inc.
- * You may not use this file except with a written agreement.
- *
- * This code is distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND,
- * EITHER EXPRESS OR IMPLIED, AND EXOLNET INC. HEREBY DISCLAIMS ALL SUCH
- * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- *
- * @package    Magasin
- * @subpackage Page
- * @author     eXolnet Inc. <info@exolnet.com>
- */
 
 use Illuminate\Support\Facades\Facade;
 
