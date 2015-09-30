d21584936dab1d161b54fd19df8dd80d50b7ad12
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2015-09-30 19:06:41 -0400

    A a route to get the site's revision

---
+++ Controller/StatusController.php
@@ -0,0 +1,45 @@
+<?php namespace Exolnet\Controller;
+
+use Controller;
+use Illuminate\Filesystem\Filesystem;
+use Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class StatusController extends Controller
+{
+	/**
+	 * @var \Illuminate\Filesystem\Filesystem
+	 */
+	private $filesystem;
+
+	/**
+	 * @param \Illuminate\Filesystem\Filesystem $filesystem
+	 */
+	public function __construct(Filesystem $filesystem)
+	{
+		$this->filesystem = $filesystem;
+	}
+
+	/**
+	 * Generic /health route to check if the application respond.
+	 *
+	 * @return \Response
+	 */
+	public function health()
+	{
+		return Response::make('OK')->header('Content-Type', 'text/plain');
+	}
+
+	public function sha()
+	{
+		$file = base_path('REVISION');
+
+		if ( ! $this->filesystem->exists($file)) {
+			throw new NotFoundHttpException;
+		}
+
+		$revision = $this->filesystem->get($file);
+
+		return Response::make($revision)->header('Content-Type', 'text/plain');
+	}
+}
--- routes.php
+++ routes.php
@@ -2,5 +2,10 @@
 
 Route::get('health', [
 	'as' => 'health',
-	'uses' => '\Exolnet\Controller\HealthController@index',
+	'uses' => '\Exolnet\Controller\StatusController@health',
+]);
+
+Route::get('sha', [
+	'as' => 'sha',
+	'uses' => '\Exolnet\Controller\StatusController@sha',
 ]);
