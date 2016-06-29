20f1d7ff41156068ff32e6263c1a90b6d191e6cb
Tom Rochette <trochette@exolnet.com>
2016-06-29 14:37:12 -0400

    Add a query helper to add subQuery to Eloquent queries

--- src/Exolnet/Database/QueryHelper.php
+++ src/Exolnet/Database/QueryHelper.php
@@ -1,9 +1,24 @@
 <?php namespace Exolnet\Database;
 
+use DB;
 use Illuminate\Database\Query\Builder;
 
 class QueryHelper
 {
+	/**
+	 * @param \Illuminate\Database\Query\Builder $query
+	 * @param string                             $alias
+	 * @return \Illuminate\Database\Query\Expression
+	 */
+	public static function subQuery(Builder $query, $alias)
+	{
+		return DB::raw('(' . $query->toSql() . ') as ' . $alias);
+	}
+
+	/**
+	 * @param \Illuminate\Database\Query\Builder $query
+	 * @return string
+	 */
 	public static function toSql(Builder $query)
 	{
 		$sql = $query->toSql();
