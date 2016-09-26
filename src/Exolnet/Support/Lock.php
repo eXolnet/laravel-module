5c77541391fae96475e39510c85428f567062c95
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2016-09-26 16:43:30 -0400

    Add a flock based lock

---
+++ src/Exolnet/Support/Lock.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Exolnet\Support;
+
+class Lock
+{
+	/**
+	 * @var string
+	 */
+	private $filename;
+
+	/**
+	 * @var resource
+	 */
+	private $handle;
+
+	/**
+	 * @var bool
+	 */
+	private $isLocked = false;
+
+	/**
+	 * @param $filename
+	 */
+	public function __construct($filename)
+	{
+		$this->filename = $filename;
+	}
+
+	/**
+	 * Unlocks on destroy.
+	 */
+	public function __destruct()
+	{
+		$this->unlock();
+	}
+
+	/**
+	 * @param bool $shouldBlock
+	 * @return bool
+	 */
+	public function lock($shouldBlock = true)
+	{
+		if ($this->isLocked) {
+			return false;
+		}
+
+		$operation = $shouldBlock ? LOCK_EX : LOCK_EX | LOCK_NB;
+
+		$this->handle = fopen($this->filename, 'w+');
+		$this->isLocked = flock($this->handle, $operation);
+
+		return $this->isLocked;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function unlock()
+	{
+		if ( ! $this->isLocked) {
+			return true;
+		}
+
+		unlink($this->filename);
+		$this->isLocked = ! flock($this->handle, LOCK_UN);
+		fclose($this->handle);
+
+		return ! $this->isLocked;
+	}
+
+	/**
+	 * @param callable $callback
+	 * @return mixed
+	 */
+	public function block(callable $callback)
+	{
+		$this->lock();
+		$value = $callback();
+		$this->unlock();
+
+		return $value;
+	}
+}
