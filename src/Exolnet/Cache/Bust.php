886fd5a64430b0c29fd0e1c00e98a17f5adc79f9
Tom Rochette <trochette@exolnet.com>
2016-09-28 10:57:55 -0400

    Add a filename getter to the Cache/Bust class.

--- src/Exolnet/Cache/Bust.php
+++ src/Exolnet/Cache/Bust.php
@@ -58,28 +58,50 @@ class Bust
 			return asset($path, $secure);
 		}
 
-		$full_path = public_path() . '/' . $path;
+		$uri = $this->path($path);
 
-		if ( ! $this->filesystem->exists($full_path)) {
-			return asset($path, $secure);
+		return asset($uri, $secure);
+	}
+
+	/**
+	 * @param string $path
+	 * @return string
+	 */
+	public function path($path)
+	{
+		if ( ! $this->isEnabled()) {
+			return $path;
 		}
 
-		$time = $this->filesystem->lastModified($full_path);
+		$fullPath = public_path() . '/' . $path;
+
+		if ( ! $this->filesystem->exists($fullPath)) {
+			return $path;
+		}
+
+		$time = $this->filesystem->lastModified($fullPath);
 
 		if ( ! $time) {
-			return asset($path, $secure);
+			return $path;
 		}
 
 		$basename = pathinfo($path, PATHINFO_BASENAME);
 		$pos = strrpos($basename, '.');
 
 		if ($pos === false) {
-			return asset($path, $secure);
+			return $path;
 		}
 
 		$basename_bust = substr($basename, 0, $pos) . '.' . $time . substr($basename, $pos);
-		$uri = substr($path, 0, -strlen($basename)) . $basename_bust;
+		return substr($path, 0, -strlen($basename)) . $basename_bust;
+	}
 
-		return asset($uri, $secure);
+	/**
+	 * @param string $path
+	 * @return string
+	 */
+	public function filename($path)
+	{
+		return basename($this->path($path));
 	}
 }
