5ec09a7ebbd48f6574ca34c0c811b457717096f1
Tom Rochette <trochette@exolnet.com>
2017-09-11 17:01:04 -0400

    Update build/split.sh to subsplit the 5.3 branch.

--- build/split.sh
+++ build/split.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 git subsplit init git@github.com:exolnet/laravelmodule.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Cache:git@github.com:exolnet/laravel-cache.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Console:git@github.com:exolnet/laravel-console.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Core:git@github.com:exolnet/laravel-core.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Database:git@github.com:exolnet/laravel-database.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Extension:git@github.com:exolnet/laravel-extension.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Formatter:git@github.com:exolnet/laravel-formatter.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Foundation:git@github.com:exolnet/laravel-foundation.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Html:git@github.com:exolnet/laravel-html.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Log:git@github.com:exolnet/laravel-log.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Menu:git@github.com:exolnet/laravel-menu.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Queue:git@github.com:exolnet/laravel-queue.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Routing:git@github.com:exolnet/laravel-routing.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Status:git@github.com:exolnet/laravel-status.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Test:git@github.com:exolnet/laravel-test.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Translation:git@github.com:exolnet/laravel-translation.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/User:git@github.com:exolnet/laravel-user.git
-git subsplit publish --heads="master 4.2 5.1 5.2" $FLAGS src/Exolnet/Validation:git@github.com:exolnet/laravel-validation.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Cache:git@github.com:exolnet/laravel-cache.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Console:git@github.com:exolnet/laravel-console.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Core:git@github.com:exolnet/laravel-core.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Database:git@github.com:exolnet/laravel-database.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Extension:git@github.com:exolnet/laravel-extension.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Formatter:git@github.com:exolnet/laravel-formatter.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Foundation:git@github.com:exolnet/laravel-foundation.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Html:git@github.com:exolnet/laravel-html.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Log:git@github.com:exolnet/laravel-log.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Menu:git@github.com:exolnet/laravel-menu.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Queue:git@github.com:exolnet/laravel-queue.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Routing:git@github.com:exolnet/laravel-routing.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Status:git@github.com:exolnet/laravel-status.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Test:git@github.com:exolnet/laravel-test.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Translation:git@github.com:exolnet/laravel-translation.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/User:git@github.com:exolnet/laravel-user.git
+git subsplit publish --heads="master 4.2 5.1 5.2 5.3" $FLAGS src/Exolnet/Validation:git@github.com:exolnet/laravel-validation.git
 rm -Rf .subsplit/
