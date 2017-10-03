6946f2d83f06f307b0f326f8841a5c899e0fe033
Tom Rochette <trochette@exolnet.com>
2017-10-03 11:49:41 -0400

    Use PHPUnit\Framework\TestCase instead of PHPUnit_Framework_TestCase when depending on PHPUnit 6 <.

--- src/Exolnet/Test/TestCaseUnit.php
+++ src/Exolnet/Test/TestCaseUnit.php
@@ -1,9 +1,9 @@
 <?php namespace Exolnet\Test;
 
 use Mockery as m;
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase;
 
-abstract class TestCaseUnit extends PHPUnit_Framework_TestCase {
+abstract class TestCaseUnit extends TestCase {
 	public function tearDown()
 	{
 		m::close();
